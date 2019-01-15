fcdfad8eace4174fe32ea968f0a0b2a1208ec886
Jarek <jarek.krupinski@snow.dog>
2019-01-15 09:15:53 +0100

    Add get-data file

---
+++ src/get-data.js
@@ -0,0 +1,75 @@
+const toArray = require('lodash/toArray')
+const sortBy = require('lodash/sortBy')
+const camelCase = require('lodash/camelCase')
+const pick = require('lodash/pick')
+
+module.exports = (content, lang, query) => {
+  const name = camelCase(query.name)
+  const type = query.type || 'collection'
+  const data = content[lang][type][name]
+  let items = []
+
+  // Move object ID inside element
+  Object.keys(data).forEach(id => {
+    if (typeof data[id] !== 'object') {
+      data[id] = {
+        id,
+        value: data[id]
+      }
+    } else if (Array.isArray(data[id])) {
+      data[id] = {
+        id,
+        items: data[id]
+      }
+    } else {
+      data[id].id = id
+    }
+  })
+
+  // Get selected items by ID
+  if (query.items) {
+    items = query.items.map(item => data[camelCase(item)])
+  }
+  else {
+    // Convert whole data to array
+    items = toArray(data)
+  }
+
+  // Sort by prop
+  if (query.sortBy) {
+    items = sortBy(items, query.sortBy)
+
+    if (query.sort === 'desc') {
+      items = items.reverse()
+    }
+  }
+
+  if (query.limit) {
+    items = items.splice(0, query.limit)
+  }
+
+  const pagination = query.page && query.perPage ? {
+    total: items.length,
+    totalPages: Math.ceil(items.length / query.perPage)
+  } : false
+
+  if (query.page && query.perPage) {
+    const start = (query.page - 1) * query.perPage
+    items = items.splice(start, query.perPage)
+  }
+
+  if (query.props) {
+    items = items.map(item => {
+      return pick(item, query.props)
+    })
+  }
+
+  if (!items.length) {
+    return null
+  }
+
+  return {
+    items,
+    pagination
+  }
+}
