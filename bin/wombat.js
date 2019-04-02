d0b335834890c2a61829d144c1a44e9ffad7fbe4
Bartek Igielski <igloczek@gmail.com>
2019-04-02 14:09:35 +0200

    CLI functions removed from main export

--- bin/wombat.js
+++ bin/wombat.js
@@ -1,14 +1,15 @@
 #!/usr/bin/env node
-const wombat = require('../index')
+const build = require('../src/build')
+const dev = require('../src/dev')
 const task = process.argv.slice(2)[0] || 'build'
 
 ;(async () => {
   switch(task) {
     case 'dev':
-      await wombat.dev()
+      await dev()
       break
     case 'build':
-      await wombat.build()
+      await build()
       break
     default:
       throw new Error('Unknown option. Only `dev` and `build` are supported.')
--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-const dev = require('./src/dev')
-const build = require('./src/build')
 const entity = require('./src/api/entity')
 const collection = require('./src/api/collection')
 
 module.exports = {
-  dev,
-  build,
   entity,
   collection
 }
