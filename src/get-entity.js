74ca5b2762bbe45e22f989efe53baf40fe7f554f
Piotr Krusicki <piotr.krusicki@snow.dog>
2022-05-31 13:17:19 +0200

    Prevent object access error

--- src/get-entity.js
+++ src/get-entity.js
@@ -4,21 +4,23 @@ const getCollection = require('./get-collection')
 
 module.exports = (content, lang, name) => {
   name = camelCase(name)
-  const entity = content[lang]['entity'][name]
+  let entity = content?.[lang]?.['entity']?.[name]
 
-  Object.keys(entity).map(prop => {
-    if (entity[prop].query) {
-      const collection = getCollection(
-        content,
-        lang,
-        entity[prop].query
-      )
+  if (entity) {
+    Object.keys(entity).map(prop => {
+      if (entity[prop].query) {
+        const collection = getCollection(
+          content,
+          lang,
+          entity[prop].query
+        )
 
-      if (collection) {
-        entity[prop] = collection.items
+        if (collection) {
+          entity[prop] = collection.items
+        }
       }
-    }
-  })
+    })
+  }
 
   return entity
 }
